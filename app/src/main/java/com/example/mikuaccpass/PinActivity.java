66702ca4f1efac7ed995f01c05e85742e1279de3
Anthony214214 <51793832+Anthony214214@users.noreply.github.com>
2020-07-05 00:23:46 +0800

    登陆操作（PinActivity注释部分）

--- app/src/main/java/com/example/mikuaccpass/PinActivity.java
+++ app/src/main/java/com/example/mikuaccpass/PinActivity.java
@@ -2,15 +2,20 @@ package com.example.mikuaccpass;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 public class PinActivity extends Activity {
     private GlobalApplication lock;
     private Button btn_main;
+//    private SharedPreferences preferences;
+//    private EditText password;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,9 +24,22 @@ public class PinActivity extends Activity {
         btn_main = findViewById(R.id.btn_main);
         lock = (GlobalApplication)getApplication();
 
+//        preferences = this.getSharedPreferences("get_pin", MODE_PRIVATE);
+//        final String MyPin = preferences.getString("Pin", "");
+
         btn_main.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+//                password = findViewById(R.id.Password);
+//                if(MyPin.equals(password)) {
+//                    Toast.makeText(PinActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+//                    Intent i = new Intent(PinActivity.this, MainActivity.class);
+//                    startActivity(i);
+//                }else{
+//                    password.setError("密码（Pin）错误，请重试！");
+//                    return;
+//                }
+
                 lock.setLocked(false);
                 PinActivity.this.finish();
             }
