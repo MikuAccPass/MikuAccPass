a1efcae52cea2e918580d56e4fa9ef13769cc0d3
Anthony214214 <51793832+Anthony214214@users.noreply.github.com>
2020-07-04 20:03:11 +0800

    设置界面事件监听（未加实际功能）

--- app/src/main/java/com/example/mikuaccpass/SettingsActivity.java
+++ app/src/main/java/com/example/mikuaccpass/SettingsActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -20,6 +25,60 @@ public class SettingsActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent i = new Intent(SettingsActivity.this, SetPinActivity.class);
                 startActivity(i);
+            }
+        });
+//开关还缺实际功能的控制
+        Switch shot_switch = (Switch)findViewById(R.id.shot_switch);
+        shot_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked){
+                    Toast shot_toast= Toast.makeText(SettingsActivity.this, "允许屏幕截图", Toast.LENGTH_SHORT);
+                    shot_toast.setGravity(Gravity.CENTER, 0, 0);
+                    shot_toast.show();
+
+                }else{
+                    Toast shot_toast = Toast.makeText(SettingsActivity.this, "禁止屏幕截图", Toast.LENGTH_SHORT);
+                    shot_toast.setGravity(Gravity.CENTER, 0, 0);
+                    shot_toast.show();
+
+                }
+            }
+        });
+
+        Switch fill_switch = (Switch)findViewById(R.id.fill_switch);
+        fill_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked){
+                    Toast fill_toast= Toast.makeText(SettingsActivity.this, "自动填充已开启", Toast.LENGTH_SHORT);
+                    fill_toast.setGravity(Gravity.CENTER, 0, 0);
+                    fill_toast.show();
+
+                }else{
+                    Toast fill_toast = Toast.makeText(SettingsActivity.this, "自动填充已关闭", Toast.LENGTH_SHORT);
+                    fill_toast.setGravity(Gravity.CENTER, 0, 0);
+                    fill_toast.show();
+
+                }
+            }
+        });
+
+        Switch fingerprint_switch = (Switch)findViewById(R.id.fingerprint_switch);
+        fingerprint_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked){
+                    Toast fingerprint_toast= Toast.makeText(SettingsActivity.this, "指纹验证已开启", Toast.LENGTH_SHORT);
+                    fingerprint_toast.setGravity(Gravity.CENTER, 0, 0);
+                    fingerprint_toast.show();
+
+                }else{
+                    Toast fingerprint_toast = Toast.makeText(SettingsActivity.this, "指纹验证已关闭", Toast.LENGTH_SHORT);
+                    fingerprint_toast.setGravity(Gravity.CENTER, 0, 0);
+                    fingerprint_toast.show();
+
+                }
             }
         });
     }
