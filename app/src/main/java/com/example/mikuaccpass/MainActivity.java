faafcce2553d93fcd09485d20805396a67e38fc6
Anthony214214 <51793832+Anthony214214@users.noreply.github.com>
2020-07-04 21:35:31 +0800

    主页跳转设置

--- app/src/main/java/com/example/mikuaccpass/MainActivity.java
+++ app/src/main/java/com/example/mikuaccpass/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mikuaccpass;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends BaseActivity {
@@ -8,6 +9,9 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView (R.layout.activity_main);
+
+        Intent i = new Intent(MainActivity.this, SettingsActivity.class);
+        startActivity(i);
     }
 }
 
