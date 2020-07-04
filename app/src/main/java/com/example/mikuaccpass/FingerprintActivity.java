9165ee8b4a7df390f332e62e45331a2d53fae897
SummerDiver <summerdiver@yeah.net>
2020-07-04 19:45:41 +0800

    指纹验证与Pin验证连接
    首选指纹验证，指纹验证多次失败或点击Pin验证按钮后跳转为Pin验证，Pin验证无法跳转至指纹验证

--- app/src/main/java/com/example/mikuaccpass/FingerprintActivity.java
+++ app/src/main/java/com/example/mikuaccpass/FingerprintActivity.java
@@ -1,8 +1,10 @@
 package com.example.mikuaccpass;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,7 +17,6 @@ import com.wei.android.lib.fingerprintidentify.base.BaseFingerprint;
 public class FingerprintActivity extends Activity {
     private LockApplication lock;
     private TextView tv_finger;
-    private Button btn_pin;
     private FingerprintIdentify identify;
 
     private static final int MAX_AVAILABLE_TIMES = 3;
@@ -27,7 +28,6 @@ public class FingerprintActivity extends Activity {
 
         lock = (LockApplication)getApplication();
         tv_finger = findViewById(R.id.tv_finger);
-        btn_pin = findViewById(R.id.btn_pin);
 
         identify = new FingerprintIdentify(getApplicationContext());
         identify.setExceptionListener(new BaseFingerprint.ExceptionListener() {
@@ -40,7 +40,8 @@ public class FingerprintActivity extends Activity {
 
         if(!identify.isFingerprintEnable()){
             tv_finger.setText("无法进行指纹验证");
-            return;
+            Intent intent = new Intent(FingerprintActivity.this, PinActivity.class);
+            FingerprintActivity.this.finish();
         }
         identify.startIdentify(MAX_AVAILABLE_TIMES, new BaseFingerprint.IdentifyListener() {
             @Override
@@ -61,9 +62,10 @@ public class FingerprintActivity extends Activity {
 
             @Override
             public void onFailed(boolean isDeviceLocked) {
-                Toast succeed_toast = Toast.makeText(getApplicationContext(),"验证失败，指纹硬件可能已被暂时锁定",Toast.LENGTH_LONG);
-                succeed_toast.setGravity(Gravity.CENTER,0,0);
-                succeed_toast.show();
+                tv_finger.setText("请使用Pin验证");
+                Intent pin = new Intent(FingerprintActivity.this, PinActivity.class);
+                startActivity(pin);
+                FingerprintActivity.this.finish();
         }
 
             @Override
@@ -88,4 +90,18 @@ public class FingerprintActivity extends Activity {
         identify.cancelIdentify();
         FingerprintActivity.this.finish();
     }
+
+    @Override
+    public void onBackPressed() {
+        Intent home = new Intent(Intent.ACTION_MAIN);
+        home.addCategory(Intent.CATEGORY_HOME);
+        startActivity(home);
+    }
+
+    public void usePin(View view){
+        Intent pin = new Intent(FingerprintActivity.this, PinActivity.class);
+        startActivity(pin);
+        FingerprintActivity.this.finish();
+    }
+
 }
