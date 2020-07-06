c9248e325cface158c76afb46c3623ae8f044c0f
SummerDiver <summerdiver@yeah.net>
2020-07-06 09:09:15 +0800

    强制设置Pin，更改资源文件名

--- app/src/main/java/com/example/mikuaccpass/MainActivity.java
+++ app/src/main/java/com/example/mikuaccpass/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.mikuaccpass;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -10,8 +12,36 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView (R.layout.activity_main);
 
-        Intent i = new Intent(MainActivity.this, SettingsActivity.class);
-        startActivity(i);
+        if(global.getPin()!=-1)
+        {
+            Intent i = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(i);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (global.getPin()==-1){
+            AlertDialog.Builder pinDialog = new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("欢迎使用Miku密码库")
+                    .setMessage("为了安全使用，请设置用于验证的Pin码")
+                    .setPositiveButton("乖乖就范", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Intent pin_set = new Intent(MainActivity.this,SetPinActivity.class);
+                            startActivity(pin_set);
+                        }
+                    })
+                    .setNegativeButton("宁可不用", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            MainActivity.this.finish();
+                        }
+                    });
+            pinDialog.show();
+        }
     }
 }
 
