48b4d577a8836725dced7e7a48951ec802539a1d
SummerDiver <summerdiver@yeah.net>
2020-07-07 16:02:44 +0800

    强制设Pin功能恢复

--- app/src/main/java/com/example/mikuaccpass/MainActivity.java
+++ app/src/main/java/com/example/mikuaccpass/MainActivity.java
@@ -1,19 +1,47 @@
-package com.example.mikuaccpass;
+        package com.example.mikuaccpass;
 
-import android.content.Intent;
-import android.os.Bundle;
+        import android.app.AlertDialog;
+        import android.content.DialogInterface;
+        import android.content.Intent;
+        import android.os.Bundle;
 
+        import androidx.annotation.Nullable;
 
-public class MainActivity extends BaseActivity {
+        public class MainActivity extends BaseActivity {
+            @Override
+            protected void onCreate(@Nullable Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView (R.layout.activity_main);
+
+                if(global.getPin()!=-1)
+                {
+                    Intent i = new Intent(MainActivity.this, SettingsActivity.class);
+                    startActivity(i);
+                }
+            }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView (R.layout.activity_main);
+    protected void onResume() {
+        super.onResume();
 
-        Intent i = new Intent(MainActivity.this, HomepageActivity.class);
-        startActivity(i);
-
+        if (global.getPin()==-1){
+            AlertDialog.Builder pinDialog = new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("欢迎使用Miku密码库")
+                    .setMessage("为了安全使用，请设置用于验证的Pin码")
+                    .setPositiveButton("乖乖就范", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Intent pin_set = new Intent(MainActivity.this,SetPinActivity.class);
+                            startActivity(pin_set);
+                        }
+                    })
+                    .setNegativeButton("宁可不用", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            MainActivity.this.finish();
+                        }
+                    });
+            pinDialog.show();
+        }
     }
 }
-
--- app/src/main/java/com/example/mikuaccpass/SplashActivity.java
+++ app/src/main/java/com/example/mikuaccpass/SplashActivity.java
@@ -48,8 +48,4 @@ public class SplashActivity extends BaseActivity {
             }
         }
     };
-
-
-
-
 }
