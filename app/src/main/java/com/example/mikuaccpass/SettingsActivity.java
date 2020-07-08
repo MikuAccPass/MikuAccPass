f75b8b0bfdf150760d0c9ea84b54bb64689b4800
SummerDiver <summerdiver@yeah.net>
2020-07-08 08:35:08 +0800

    “允许截图”开关bug修复

--- app/src/main/java/com/example/mikuaccpass/SettingsActivity.java
+++ app/src/main/java/com/example/mikuaccpass/SettingsActivity.java
@@ -20,6 +20,7 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
+
 import androidx.fragment.app.Fragment;
 
 public class SettingsActivity extends Fragment {
@@ -28,13 +29,11 @@ public class SettingsActivity extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState)
-    {
+                             ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.activity_settings, container, false);
-        global = (GlobalApplication)getActivity().getApplication();
+        global = (GlobalApplication) getActivity().getApplication();
         preferences = getActivity().getSharedPreferences("setting", Context.MODE_PRIVATE);
-        getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
 
         Button pin_btn = root.findViewById(R.id.pin_btn);
         pin_btn.setOnClickListener(new View.OnClickListener() {
@@ -51,28 +50,30 @@ public class SettingsActivity extends Fragment {
         shot_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                if(isChecked){
+                if (isChecked) {
                     getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SECURE);
-                }else{
+                } else {
                     getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
                 }
+                global.setScreenshotPermit(isChecked);
             }
         });
 
         final Switch fill_switch = root.findViewById(R.id.fill_switch);
-        fill_switch.setChecked(preferences.getBoolean("autofill_enable",false));
+        fill_switch.setChecked(preferences.getBoolean("autofill_enable", false));
         fill_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                if(isChecked){ if(Build.VERSION.SDK_INT<26){
-                    fill_switch.setChecked(false);
-                    Toast fill_toast = Toast.makeText(getActivity(), "该功能需要安卓8.0以上支持", Toast.LENGTH_SHORT);
-                    fill_toast.setGravity(Gravity.CENTER, 0, 0);
-                    fill_toast.show();
-                    return;
-                }
-                    if(!getActivity().getSystemService(AutofillManager.class).hasEnabledAutofillServices()){
-                        AlertDialog.Builder pinDialog = new AlertDialog.Builder(getActivity())
+                if (isChecked) {
+                    if (Build.VERSION.SDK_INT < 26) {
+                        fill_switch.setChecked(false);
+                        Toast fill_toast = Toast.makeText(getActivity(), "该功能需要安卓8.0以上支持", Toast.LENGTH_SHORT);
+                        fill_toast.setGravity(Gravity.CENTER, 0, 0);
+                        fill_toast.show();
+                        return;
+                    }
+                    if (!getActivity().getSystemService(AutofillManager.class).hasEnabledAutofillServices()) {
+                        AlertDialog.Builder autofillDialog = new AlertDialog.Builder(getActivity())
                                 .setCancelable(false)
                                 .setTitle("自动填充服务未启动")
                                 .setMessage("为了正常使用，请前往设置-系统-语言和输入法-高级-自动填充服务，选择“Miku Autofill Service”。")
@@ -88,14 +89,13 @@ public class SettingsActivity extends Fragment {
                                         fill_switch.setChecked(false);
                                     }
                                 });
-                        pinDialog.show();
-                    }
-                    else {
+                        autofillDialog.show();
+                    } else {
                         SharedPreferences.Editor editor = preferences.edit();
                         editor.putBoolean("autofill_enable", true);
                         editor.apply();
                     }
-                }else{
+                } else {
                     SharedPreferences.Editor editor = preferences.edit();
                     editor.putBoolean("autofill_enable", false);
                     editor.apply();
@@ -108,15 +108,14 @@ public class SettingsActivity extends Fragment {
         fingerprint_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                if (global.getPin()==-1){
+                if (global.getPin() == -1) {
                     fingerprint_switch.setChecked(false);
                     Toast fill_toast = Toast.makeText(getActivity(), "请先设置Pin", Toast.LENGTH_SHORT);
                     fill_toast.setGravity(Gravity.CENTER, 0, 0);
                     fill_toast.show();
                     Intent pin_set = new Intent(getActivity(), SetPinActivity.class);
                     startActivity(pin_set);
-                }
-                else {
+                } else {
                     SharedPreferences.Editor editor = preferences.edit();
                     editor.putBoolean("fingerprint_enable", true);
                     editor.apply();
