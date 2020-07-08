c23b4423bcb29aaafadad6fa7fc1d421054e0883
ThreeFire1 <64344160+ThreeFire1@users.noreply.github.com>
2020-07-08 08:53:46 +0800

    自动生成密码
    自动生成密码

--- app/src/main/java/com/example/mikuaccpass/RecordActivity.java
+++ app/src/main/java/com/example/mikuaccpass/RecordActivity.java
@@ -12,6 +12,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.Random;
+
 
 public class RecordActivity extends BaseActivity {
 
@@ -70,5 +72,29 @@ public class RecordActivity extends BaseActivity {
         startActivity(new Intent(RecordActivity.this, MainActivity.class));
     }
 
+    //随机密码填充
+    public void RandomNum(View v){
+        String password=genRandomNum();
+        et_password.setText(password);
 
+    }
+    //获取随机密码
+    public String genRandomNum() {
+        int maxNum = 36;
+        int i;
+        int count = 0;
+        char[] str = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+                'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+                'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        StringBuffer pwd = new StringBuffer("");
+        Random r = new Random();
+        while (count < 8) {
+            i = Math.abs(r.nextInt(maxNum));
+            if (i >= 0 && i < str.length) {
+                pwd.append(str[i]);
+                count++;
+            }
+        }
+        return pwd.toString();
+    }
 }
